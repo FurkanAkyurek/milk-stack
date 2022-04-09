07b6db5fd256e6256a26643c071c9b183b8932b7
Furkan Akyürek <furkan.akyurek23@gmail.com>
2022-04-09 12:33:43 +0300

    fix

--- Assets/Scripts/Capping_Machine.cs
+++ Assets/Scripts/Capping_Machine.cs
@@ -2,20 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collectible : MonoBehaviour
+public class Capping_Machine : MonoBehaviour
 {
+    // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    // Update is called once per frame
     void Update()
     {
         
     }
-}
-
-public enum Status
-{
-
 }
---
+++ Assets/Scripts/FinishStep.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using Zenta.Core.Runtime.Managers;
+
+public class FinishStep : MonoBehaviour
+{
+    [SerializeField] private Transform milkTarget;
+
+    public float multiplyAmount;
+
+    public bool isPassable = false;
+    private bool isPassed = false;
+
+    private void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (!isPassed)
+            {
+                if (isPassable && multiplyAmount != 12)
+                {
+                    
+                }
+                else if (!isPassable || multiplyAmount == 12)
+                {
+                    StartCoroutine(FinishSequenceDelay());
+                }
+
+                isPassed = true;
+            }
+        }
+    }
+
+    IEnumerator FinishSequenceDelay()
+    {
+        yield return new WaitForSeconds(1f);
+
+
+        yield return new WaitForSeconds(2f);
+        
+        GameManager.Instance.CompleteLevel();
+    }
+}
